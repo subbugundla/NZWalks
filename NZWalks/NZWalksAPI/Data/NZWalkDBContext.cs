3adea5f358cfafee88eeea3619bf1ed0110396af
subbu <subbu.gundla@gmail.com>
2023-03-06 13:00:22 -0500

    Created Db Tables

---
+++ NZWalks/NZWalksAPI/Data/NZWalkDBContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NZWalksAPI.Models.Domain;
+
+namespace NZWalksAPI.Data
+{
+    public class NZWalkDBContext:DbContext
+    {
+        public NZWalkDBContext(DbContextOptions<NZWalkDBContext> options): base(options)
+        {
+
+        }
+
+        public DbSet<Region> Regions { get; set; }
+        public  DbSet<Walk> Walks { get; set; }
+        public  DbSet<WalkDifficulty> WalkDifficulty { get; set; }  
+    }
+}
